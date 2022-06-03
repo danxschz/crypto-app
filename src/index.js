15386ef0a78ba2dca6d2d7f2a1e02b2b744c102f
Daniel Sanchez <danxschz@gmail.com>
2022-06-03 18:01:24 -0400

    Style header and set date display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import moment from 'moment';
 import './normalize.css';
 import './style.scss';
+
+const date = document.querySelector('.date');
+date.textContent = moment().format("dddd, MMMM Do YYYY");
 
 const getCoins = async () => {
   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false', { mode: 'cors' });
