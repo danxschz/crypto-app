6fe15da8f82070af328e69b236a1a58e8bf3c090
Daniel Sanchez <danxschz@gmail.com>
2022-06-03 00:33:21 -0400

    Add basic styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './normalize.css';
+import './style.scss';
+
 const getCoins = async () => {
   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false', { mode: 'cors' });
   return response.json();
@@ -63,7 +66,7 @@ const displayIndicators = (indicators) => {
   let i = 0
 
   // Get top 2 coins
-  for (coin in dominanceData) {
+  for (let coin in dominanceData) {
     if (i >= 2) break;
     dominanceValue += ` ${coin}: ${dominanceData[coin].toFixed(1)}%`
     i += 1;
